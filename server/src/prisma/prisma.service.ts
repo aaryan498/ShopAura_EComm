c643d61860fc589fa6d63981a42cee108c0b0e7c
Aaryan Kumar <kumaraaryan498@gmail.com>
2026-07-14 02:20:17 +0530

    synced database, connected and started database from server

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -1,6 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-    
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+
+    constructor() {
+        const adapter = new PrismaPg({
+            connectionString: process.env.DATABASE_URL,
+        })
+
+        super({
+            adapter,
+            log: process.env.NODE_ENV == 'development'
+                ? ["query", "error", "warn"]
+                : ["error"],
+        })
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+        console.log("Database [CONNECTED]");
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
+        console.log("Database [DISCONNECTED]")
+    }
+
+    async cleanDatabase() {
+        if(process.env.NODE_ENV === 'production'){
+            return new Error("Database Cleaning RESTRICTED for produnction");
+        }
+
+        const models = Reflect.ownKeys(this).filter(
+            (key) => typeof key === 'string' && !key.startsWith('_'),
+        );
+
+        return Promise.all(
+            models.map((modelKey) => {
+                if(typeof modelKey === 'string'){
+                    return this[modelKey].deleteMany();
+                }
+            })
+        )
+    }
+
+
+
 }
