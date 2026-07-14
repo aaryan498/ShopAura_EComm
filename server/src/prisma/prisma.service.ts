541e8bf0b62912b499958c152fd6c81e96c6c028
Aaryan Kumar <kumaraaryan498@gmail.com>
2026-07-14 11:57:13 +0530

    fix : all prisma errors

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
