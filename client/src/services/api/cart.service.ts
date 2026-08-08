d5d1d1d20ebd2e56370f2fc3b17356ee73949a1e
Aaryan <kumaraaryan498@gmail.com>
2026-08-08 17:17:50 +0530

    Update CartService endpoint and methods

--- client/src/services/api/cart.service.ts
+++ client/src/services/api/cart.service.ts
@@ -1,20 +1,49 @@
 import { CartResponse } from "@/types/cart.types";
-import { ProductCart } from "@/types/product.types";
 import { apiClient } from "./axios.config";
-
 
 export class CartService {
 
-    private static readonly ENDPOINT = '/cart';
+    private static readonly ENDPOINT = '/carts';
 
+    static async getCurrentCart(): Promise<CartResponse> {
+        const response = await apiClient.get<CartResponse>(
+            this.ENDPOINT
+        );
 
-    static async mergeCart(localCart: ProductCart[]): Promise<CartResponse>{
+        return response.data;
+    }
 
+    static async addProduct(productId: string): Promise<CartResponse> {
         const response = await apiClient.post<CartResponse>(
-            `${this.ENDPOINT}/merge`, 
-            {
-                items: localCart,
-            }   
+            `${this.ENDPOINT}/${productId}`
+        );
+
+        return response.data;
+    }
+
+    static async updateProductQuantity(
+        productId: string,
+        quantity: number
+    ): Promise<CartResponse> {
+        const response = await apiClient.patch<CartResponse>(
+            `${this.ENDPOINT}/${productId}`,
+            { quantity }
+        );
+
+        return response.data;
+    }
+
+    static async removeProduct(productId: string): Promise<CartResponse> {
+        const response = await apiClient.delete<CartResponse>(
+            `${this.ENDPOINT}/${productId}`
+        );
+
+        return response.data;
+    }
+
+    static async clearCart(): Promise<CartResponse> {
+        const response = await apiClient.delete<CartResponse>(
+            this.ENDPOINT
         );
 
         return response.data;
